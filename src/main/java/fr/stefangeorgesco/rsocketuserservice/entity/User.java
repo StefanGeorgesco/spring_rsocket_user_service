ccfaf552fb1b91ee00c9be333bac68ec64a0e193
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-08-27 09:54:21 +0200

    add entity

---
+++ src/main/java/fr/stefangeorgesco/rsocketuserservice/entity/User.java
@@ -0,0 +1,44 @@
+package fr.stefangeorgesco.rsocketuserservice.entity;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private int balance;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
