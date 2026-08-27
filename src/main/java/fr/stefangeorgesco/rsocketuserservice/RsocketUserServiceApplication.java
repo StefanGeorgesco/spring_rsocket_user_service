b604fcbfe12f624302248b7637a7b0b5fd8b73e6
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-08-27 09:49:19 +0200

    initial commit

---
+++ src/main/java/fr/stefangeorgesco/rsocketuserservice/RsocketUserServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.stefangeorgesco.rsocketuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocketUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/stefangeorgesco/rsocketuserservice/RsocketUserServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package fr.stefangeorgesco.rsocketuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Test that the application context loads successfully
+	}
+
+}
